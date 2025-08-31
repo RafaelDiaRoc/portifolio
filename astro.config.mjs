2049bfad7ea5ba3ba79913b833b5f50bb077da3b
Rafael Diana Rocha <rafael.rocha@infomanaiweb.com>
2025-08-31 19:07:51 -0300

    Final Fix: Reconfigura integrações para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,7 @@ import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [
-    tailwind({
-      // Desativa a aplicação de estilos base padrão do Tailwind, se necessário.
-      // applyBaseStyles: false,
-    }), 
-    icon({
-      // Especifica qual pacote de ícones usar
-      iconDir: 'node_modules/@iconify-json/mdi/icons.json',
-      include: {
-        mdi: ['*']
-      }
-    })
+    tailwind(), 
+    icon()
   ]
 });
