29ae50be3d9dac824ba69502e6ab5d80be818658
Rafael Diana Rocha <rafael.rocha@infomanaiweb.com>
2025-08-31 14:18:47 -0300

    Versão final do portfólio

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        }
+      },
+      animation: {
+        'gradient-bg': 'gradient 15s ease infinite',
+      }
+    },
+  },
+  plugins: [],
+}
